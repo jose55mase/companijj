ab0116ae458f281dd18e4ad958abbbeac9ccd821
joxe luis <josemase55pp@gmail.com>
2021-03-10 16:42:19 -0500

    Desarrollo login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './../../service/login_service/login.service'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  fakeLogin = {
+    name:"",
+    password:""
+  }
+
+  constructor(private loginService$:LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  onCancelCloseView(){
+    this.loginService$.loginStateView.emit(false)
+  }
+
+  onLogin(){
+    console.log(this.fakeLogin)
   }
 
 }
